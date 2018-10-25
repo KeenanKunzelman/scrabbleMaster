488cb3a0f3a5fd4f08a2c3049f5d4271167f5862
KeenanKunzelman <keekunz@gmail.com>
2018-10-25 15:46:46 -0400

    Added cell class

---
+++ src/cell.java
@@ -0,0 +1,39 @@
+
+public class cell {
+	String value;
+	String row;
+	String column;
+	Boolean isTraversed;
+	
+	public cell(String value, String row, String column, Boolean isTraversed) {
+		this.value = value;
+		this.row = row;
+		this.column = column;
+		this.isTraversed = isTraversed;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getRow() {
+		return row;
+	}
+	public void setRow(String row) {
+		this.row = row;
+	}
+	public String getColumn() {
+		return column;
+	}
+	public void setColumn(String column) {
+		this.column = column;
+	}
+	public Boolean getIsTraversed() {
+		return isTraversed;
+	}
+	public void setIsTraversed(Boolean isTraversed) {
+		this.isTraversed = isTraversed;
+	}
+}
